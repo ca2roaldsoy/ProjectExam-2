7b5608f59ce9bc63db33da0970c8d0d3150e3382
ca2roaldsoy <roaldsoyen@gmail.com>
2020-05-11 17:40:11 +0200

    added directories and api

---
+++ src/constants/api.js
@@ -0,0 +1,8 @@
+const KEY = 5ea81ad23851a633426eb802;
+const Base_URL = "https://us-central1-noroff-final-exam.cloudfunctions.net/api/";
+const FETCH_OPTIONS = {
+    headers: {
+        "Content-Type": "application/json",
+        key: KEY
+    }
+}
