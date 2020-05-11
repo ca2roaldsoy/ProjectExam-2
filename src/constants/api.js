3ec42a94fdb54f3e4f8487aaeca1a5339e758c4b
ca2roaldsoy <roaldsoyen@gmail.com>
2020-05-11 17:51:16 +0200

    changed key to string

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,8 +1,9 @@
-const KEY = 5ea81ad23851a633426eb802;
-const Base_URL = "https://us-central1-noroff-final-exam.cloudfunctions.net/api/";
+const KEY = "5ea81ad23851a633426eb802";
+const Base_URL =
+  "https://us-central1-noroff-final-exam.cloudfunctions.net/api/";
 const FETCH_OPTIONS = {
-    headers: {
-        "Content-Type": "application/json",
-        key: KEY
-    }
-}
+  headers: {
+    "Content-Type": "application/json",
+    key: KEY,
+  },
+};
