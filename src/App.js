e47c1b27bc857309781095d4ee0c6008451ec2c0
ca2roaldsoy <roaldsoyen@gmail.com>
2020-05-11 18:34:08 +0200

    displaying menu on homepage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles/style.scss";
+import NavMenu from "./components/menu/NavMenu";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavMenu />
     </div>
   );
 }
--- src/components/menu/NavMenu.js
+++ src/components/menu/NavMenu.js
@@ -9,17 +9,53 @@ import {
   NavLink,
 } from "react-router-dom";
 import Home from "../visitorSite/home/Home.js";
-import Contact from "../visitorSite/contact/Contact.js";
+/*import Contact from "../visitorSite/contact/Contact.js";
 import Admin from "../adminSite/Admin.js";
 import HotelList from "../visitorSite/hotelList";
 import HotelSpecific from "../visitorSite/hotelSpecific";
 import MakeEnquiries from "../visitorSite/makeEnquiries";
 import newEstablishment from "../adminSite/newEstablishment";
 import viewEnquiries from "../adminSite/viewEnquiries";
-import ContactMsg from "../adminSite/contactMsg";
+import ContactMsg from "../adminSite/contactMsg";*/
 
+// Navigation Menu
 function NavMenu() {
-  return <div></div>;
+  return (
+    <Router>
+      <Navbar role="navigation" expand="md">
+        <Navbar.Brand>
+          <NavLink to="/" exact role="link">
+            Holidaze
+          </NavLink>
+        </Navbar.Brand>
+
+        {/* hamburger menu */}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <NavLink to="/" exact className="nav-link" role="link">
+              Home
+            </NavLink>
+            <NavLink to="/contact" className="nav-link" role="link">
+              Contact
+            </NavLink>
+            <NavLink to="/admin" className="nav-link" role="link">
+              Admin
+            </NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
+      {/* Navigation showing correct component */}
+      <Container>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          {/*<Route path="/contact" component={Contact} /> */}
+          {/*<Route path="/admin" component={Admin} /> */}
+        </Switch>
+      </Container>
+    </Router>
+  );
 }
 
 export default NavMenu;
