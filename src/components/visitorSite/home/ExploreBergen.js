1725b7d74b907bfc911b173419940230bbcfe9c1
ca2roaldsoy <roaldsoyen@gmail.com>
2020-05-12 19:59:18 +0200

    added images to home page

---
+++ src/components/visitorSite/home/ExploreBergen.js
@@ -0,0 +1,48 @@
+import React from "react";
+import Carousel from "react-multi-carousel";
+import { BergenImg } from "./BergenImg";
+import BergenCarousel from "./BergenCarousel";
+
+function ExploreBergen() {
+  const responsive = {
+    lg: {
+      breakpoint: { max: 2048, min: 1024 },
+      items: 3,
+      slidesToSlide: 1,
+    },
+    md: {
+      breakpoint: { max: 1024, min: 768 },
+      items: 2,
+      slidesToSlide: 1,
+    },
+    sm: {
+      breakpoint: { max: 768, min: 0 },
+      items: 1,
+      slidesToSlide: 1,
+    },
+  };
+
+  function bergenImg() {
+    return BergenImg.map((images, i) => {
+      const { src, alt, text, imgTitle } = images;
+
+      return (
+        <BergenCarousel
+          key={i}
+          imgTitle={imgTitle}
+          image={src}
+          alt={alt}
+          text={text}
+        />
+      );
+    });
+  }
+
+  return (
+    <Carousel responsive={responsive} showDots={true}>
+      {bergenImg()}
+    </Carousel>
+  );
+}
+
+export default ExploreBergen;
--- src/components/visitorSite/home/Home.js
+++ src/components/visitorSite/home/Home.js
@@ -1,47 +1,17 @@
 import React from "react";
-import Carousel from "react-multi-carousel";
-import { BergenImg } from "./BergenImg";
-import BergenCarousel from "./BergenCarousel";
+import ExploreBergen from "./ExploreBergen";
+import ImgTop from "../../../images/bergen/bg_img.jpg";
+import ImgBlur from "../../../images/bergen/bergen_blur.jpg";
+import Image from "react-bootstrap/Image";
 
 function Home() {
-  const responsive = {
-    lg: {
-      breakpoint: { max: 2048, min: 1024 },
-      items: 3,
-      slidesToSlide: 1,
-    },
-    md: {
-      breakpoint: { max: 1024, min: 768 },
-      items: 2,
-      slidesToSlide: 1,
-    },
-    sm: {
-      breakpoint: { max: 768, min: 0 },
-      items: 1,
-      slidesToSlide: 1,
-    },
-  };
-
-  function bergenImg() {
-    return BergenImg.map((images, i) => {
-      const { src, alt, text, imgTitle } = images;
-
-      return (
-        <BergenCarousel
-          key={i}
-          imgTitle={imgTitle}
-          image={src}
-          alt={alt}
-          text={text}
-        />
-      );
-    });
-  }
-
   return (
-    <Carousel responsive={responsive} showDots={true}>
-      {bergenImg()}
-    </Carousel>
+    <>
+      <Image src={ImgTop} alt="man on hike looking over Bergen" fluid />
+      <Image src={ImgBlur} alt="panorama view over Bergen" fluid />
+      <h2>Explore Bergen</h2>
+      <ExploreBergen />
+    </>
   );
 }
 
