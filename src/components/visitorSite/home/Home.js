67aa1516d040396009e00d3ce7c42894b7a0029f
ca2roaldsoy <roaldsoyen@gmail.com>
2020-05-14 10:17:58 +0200

    add and filter api

--- src/components/visitorSite/home/Home.js
+++ src/components/visitorSite/home/Home.js
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ExploreBergen from "./ExploreBergen";
 import ImgTop from "../../../images/bergen/bg_img_v2.jpg";
 import ImgBlur from "../../../images/bergen/bergen_blur_v2.jpg";
 import Image from "react-bootstrap/Image";
 import Footer from "../footer/Footer";
+import { BASE_URL, FETCH_OPTIONS } from "../../../constants/api";
 
 function Home() {
+  const [establishments, setEstablishments] = useState([]);
+  const [searchEstablishments, setSearchEstablishments] = useState([]);
+
+  const url = BASE_URL + "establishments";
+
+  useEffect(() => {
+    fetch(url, FETCH_OPTIONS)
+      .then((response) => response.json())
+      .then((data) => {
+        setEstablishments(data);
+        setSearchEstablishments(data);
+        console.log(data);
+      })
+      .catch((err) => console.log(err));
+  }, [url]);
+
+  const findEstablishment = function (e) {
+    const lowerCaseValue = e.target.value.toLowerCase();
+    const filterEstablishments = establishments.filter((establishment) => {
+      const lowerCaseEst = establishment.toLowerCase();
+
+      if (lowerCaseEst.includes(lowerCaseValue)) {
+        return true;
+      }
+      return false;
+    });
+    setSearchEstablishments(filterEstablishments);
+  };
+
   return (
     <>
       <Image src={ImgTop} alt="man on hike looking over Bergen" fluid />
