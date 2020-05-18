2d5b56f4f7590e54387c44e253aadc5de59bc172
ca2roaldsoy <roaldsoyen@gmail.com>
2020-05-18 18:24:44 +0200

    inputet files from components to constants folder

---
+++ src/constants/responsiveCarousel.js
@@ -0,0 +1,17 @@
+export const Responsive = {
+  lg: {
+    breakpoint: { max: 2048, min: 1024 },
+    items: 3,
+    slidesToSlide: 1,
+  },
+  md: {
+    breakpoint: { max: 1024, min: 768 },
+    items: 2,
+    slidesToSlide: 1,
+  },
+  sm: {
+    breakpoint: { max: 768, min: 0 },
+    items: 1,
+    slidesToSlide: 1,
+  },
+};
