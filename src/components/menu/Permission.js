8ab7d22e4b5a6727b73587441a753beda10905ed
ca2roaldsoy <roaldsoyen@gmail.com>
2020-05-20 10:54:11 +0200

    added permission image and bootstraÃp image overlay

--- src/components/menu/Permission.js
+++ src/components/menu/Permission.js
@@ -1,14 +1,27 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
+import StopSign from "../../images/stop.png";
+import Card from "react-bootstrap/Card";
 
 function Permission() {
   const history = useHistory();
 
   setTimeout(() => {
     history.push("/");
-  }, 2000);
+  }, 5000);
 
-  return <div>You dont have permission</div>;
+  return (
+    <Card>
+      <Card.Img src={StopSign} alt="man denying access" role="image" />
+      <Card.ImgOverlay>
+        <Card.Title>
+          Sorry, You dont have permission to view this page
+        </Card.Title>
+        <Card.Text>Please log in to gain access to this page</Card.Text>
+        <Card.Text>You will now be redirectd to home page</Card.Text>
+      </Card.ImgOverlay>
+    </Card>
+  );
 }
 
 export default Permission;
