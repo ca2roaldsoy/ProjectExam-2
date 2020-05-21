5209360de028b5fd2f5b61bb93255afa03f771d7
ca2roaldsoy <roaldsoyen@gmail.com>
2020-05-21 21:05:33 +0200

    fetched enquiries from visitor

--- src/components/adminSite/enquiries/Enquiries.js
+++ src/components/adminSite/enquiries/Enquiries.js
@@ -1,23 +1,64 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { BASE_URL, headers } from "../../../constants/api";
 import EnquiryDetails from "./EnquiriesDetails";
+import Loading from "../../spinner/Loading";
+import CardDeck from "react-bootstrap/CardDeck";
 
 function Enquiries() {
+  const [enquiry, setEnquiry] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   const url = BASE_URL + "enquiries";
   const options = { headers };
 
   useEffect(() => {
     fetch(url, options)
       .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.log(err));
+      .then((data) => {
+        setEnquiry(data);
+        console.log(data);
+      })
+      .catch((err) => console.log(err))
+      .finally(setLoading(false));
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  // use spinner
+  if (loading) {
+    return <Loading />;
+  }
+
+  function noEnquiries() {
+    if (enquiry.length === 0) {
+      return <p>There is no Enquiry yet</p>;
+    }
+    return enquiry.map((enq, i) => {
+      const {
+        name,
+        email,
+        establishmentId,
+        checkIn,
+        checkOut,
+        createdAt,
+      } = enq;
+      return (
+        <EnquiryDetails
+          name={name}
+          email={email}
+          id={establishmentId}
+          key={i}
+          checkIn={checkIn}
+          checkOut={checkOut}
+          created={createdAt}
+        />
+      );
+    });
+  }
 
   return (
     <>
       <h1>Enquiries</h1>
-      <EnquiryDetails />
+      <CardDeck>{noEnquiries()}</CardDeck>
     </>
   );
 }
--- src/components/adminSite/enquiries/EnquiriesDetails.js
+++ src/components/adminSite/enquiries/EnquiriesDetails.js
@@ -2,22 +2,26 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
 
-function EnquiriesDetails() {
+function EnquiriesDetails({ name, email, id, checkIn, checkOut, created }) {
   return (
-    <Card>
-      <Card.Title>Name</Card.Title>
-      <Card.Body>
-        <ListGroup variant="flush">
-          <ListGroup.Item>Name:</ListGroup.Item>
-          <ListGroup.Item>E-mail:</ListGroup.Item>
-          <ListGroup.Item>Check In:</ListGroup.Item>
-          <ListGroup.Item>Check Out:</ListGroup.Item>
-        </ListGroup>
-      </Card.Body>
-      <Button>Accept</Button>
-      <Button>Reject</Button>
-    </Card>
+    <Col>
+      <Card>
+        <Card.Title>{id}</Card.Title>
+        <Card.Body>
+          <ListGroup variant="flush">
+            <ListGroup.Item>Name: {name}</ListGroup.Item>
+            <ListGroup.Item>E-mail: {email} </ListGroup.Item>
+            <ListGroup.Item>Check In: {checkIn.slice(0, 10)} </ListGroup.Item>
+            <ListGroup.Item>Check Out: {checkOut.slice(0, 10)} </ListGroup.Item>
+          </ListGroup>
+        </Card.Body>
+        <Card.Footer>{created.slice(0, 10)}</Card.Footer>
+        <Button>Accept</Button>
+        <Button>Reject</Button>
+      </Card>
+    </Col>
   );
 }
 
