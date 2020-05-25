0791d7356f85259432a45f27ad23a921408901b8
ca2roaldsoy <roaldsoyen@gmail.com>
2020-05-25 20:59:19 +0200

    connection with visitor site

--- src/components/adminSite/contactMsg/Messages.js
+++ src/components/adminSite/contactMsg/Messages.js
@@ -1,19 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import CardDeck from "react-bootstrap/CardDeck";
 import { BASE_URL, headers } from "../../../constants/api";
+import MessagesLayout from "./MessagesLayout";
 
 function Messages() {
+  const [contactMsg, setContactMsg] = useState([]);
+
   const url = BASE_URL + "contacts";
   const options = { headers };
 
   useEffect(() => {
     fetch(url, options)
       .then((repsonse) => repsonse.json())
-      .then((data) => console.log(data))
+      .then((data) => {
+        setContactMsg(data);
+        console.log(data);
+      })
       .catch((err) => console.log(err));
-  });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  return <div>hello</div>;
+  function noMsg() {
+    if (contactMsg.length === 0) {
+      return <p>No messages</p>;
+    }
+    return contactMsg.map((contact) => {
+      const { name, email, message } = contact;
+      return <MessagesLayout name={name} email={email} message={message} />;
+    });
+  }
+
+  return <CardDeck>{noMsg}</CardDeck>;
 }
 
 export default Messages;
--- src/components/adminSite/contactMsg/MessagesLayout.js
+++ src/components/adminSite/contactMsg/MessagesLayout.js
@@ -3,13 +3,13 @@ import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 import Button from "react-bootstrap/Button";
 
-function MessagesLayout() {
+function MessagesLayout({ name, email, message }) {
   return (
     <Card>
       <ListGroup variant="flush">
-        <ListGroup.Item>From: </ListGroup.Item>
-        <ListGroup.Item>E-mail: </ListGroup.Item>
-        <ListGroup.Item>Message: </ListGroup.Item>
+        <ListGroup.Item>From: {name}</ListGroup.Item>
+        <ListGroup.Item>E-mail: {email}</ListGroup.Item>
+        <ListGroup.Item>Message: {message}</ListGroup.Item>
       </ListGroup>
       <Button>Respond</Button>
     </Card>
