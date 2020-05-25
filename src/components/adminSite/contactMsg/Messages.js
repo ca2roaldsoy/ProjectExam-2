5fce9c3772c39516a7cefd2e5a91c721ec609c20
ca2roaldsoy <roaldsoyen@gmail.com>
2020-05-25 20:48:30 +0200

    fetch api

--- src/components/adminSite/contactMsg/Messages.js
+++ src/components/adminSite/contactMsg/Messages.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import CardDeck from "react-bootstrap/CardDeck";
+import { BASE_URL, headers } from "../../../constants/api";
 
 function Messages() {
+  const url = BASE_URL + "contacts";
+  const options = { headers };
+
+  useEffect(() => {
+    fetch(url, options)
+      .then((repsonse) => repsonse.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  });
+
   return <div>hello</div>;
 }
 
