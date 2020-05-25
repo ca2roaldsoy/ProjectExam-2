99f4fc628955a63b041cf0ab265d1bb8b2d77a36
ca2roaldsoy <roaldsoyen@gmail.com>
2020-05-25 21:55:15 +0200

    added bold text

--- src/components/adminSite/contactMsg/MessagesLayout.js
+++ src/components/adminSite/contactMsg/MessagesLayout.js
@@ -1,16 +1,21 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
-import Button from "react-bootstrap/Button";
 import MessageRespond from "./MessageRespond";
 
 function MessagesLayout({ name, email, message }) {
   return (
     <Card>
       <ListGroup variant="flush">
-        <ListGroup.Item>From: {name}</ListGroup.Item>
-        <ListGroup.Item>E-mail: {email}</ListGroup.Item>
-        <ListGroup.Item>Message: {message}</ListGroup.Item>
+        <ListGroup.Item>
+          <b>From:</b> {name}
+        </ListGroup.Item>
+        <ListGroup.Item>
+          <b>E-mail:</b> {email}
+        </ListGroup.Item>
+        <ListGroup.Item>
+          <b>Message:</b> {message}
+        </ListGroup.Item>
       </ListGroup>
       <MessageRespond name={name} email={email} />
     </Card>
