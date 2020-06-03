881ceb1bf343dcaea63332f4d83272ca82b59f67
ca2roaldsoy <roaldsoyen@gmail.com>
2020-06-03 13:26:27 +0200

    added qualities to footer

--- src/components/visitorSite/footer/Footer.js
+++ src/components/visitorSite/footer/Footer.js
@@ -3,12 +3,48 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import FacebookIcon from "../../../images/icons/face_v1.png";
 import TwitterIcon from "../../../images/icons/twitter_v1.png";
+import acceptIcon from "../../../images/accept.png";
 import Image from "react-bootstrap/Image";
 import FooterContact from "./FooterContact";
 
 function Footer() {
   return (
     <Row as="footer" role="contentinfo" className="footer">
+      <Col sm={12} className="footer__top">
+        <Col sm={4}>
+          <Image
+            src={acceptIcon}
+            fluid
+            className="footer__top--img img-responsive"
+            role="img"
+            alt="letter v inside green circle"
+          />
+          <p className="footer__top--text">No booking fee</p>
+        </Col>
+
+        <Col sm={4}>
+          <Image
+            src={acceptIcon}
+            fluid
+            className="footer__top--img img-responsive"
+            role="img"
+            alt="letter v inside green circle"
+          />
+          <p className="footer__top--text">100&amp; Satisfaction</p>
+        </Col>
+
+        <Col sm={4}>
+          <Image
+            src={acceptIcon}
+            fluid
+            className="footer__top--img img-responsive"
+            role="img"
+            alt="letter v inside green circle"
+          />
+          <p className="footer__top--text">Located in Bergen</p>
+        </Col>
+      </Col>
+
       <Col sm={12} md={3} lg={3} as="section" className="footer__title">
         <h1 className="footer__title--text">Holidaze</h1>
       </Col>
