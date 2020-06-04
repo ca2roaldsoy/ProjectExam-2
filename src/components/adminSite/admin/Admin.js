e03f0113c9deb5d7b276d068c1845603c08b2b5c
ca2roaldsoy <roaldsoyen@gmail.com>
2020-06-04 18:40:18 +0200

    styled admin page and made it responsive

--- src/components/adminSite/admin/Admin.js
+++ src/components/adminSite/admin/Admin.js
@@ -5,28 +5,31 @@ import { Link } from "react-router-dom";
 import AdminImg from "../../../images/bergen/admin_page.jpg";
 import Image from "react-bootstrap/Image";
 import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
 
 function Admin() {
   return (
-    <Row>
-      <Col lg={6}>
-        <h1>Holidaze</h1>
+    <Container className="adminContainer">
+      <Row className="admin">
+        <Col lg={6} className="admin__section">
+          <h1 className="admin__section--title">Holidaze</h1>
 
-        <Link to="/enquiries">
-          <Button role="button">Enquiries</Button>
-        </Link>
-        <Link to="./messages">
-          <Button role="button">Messages</Button>
-        </Link>
-        <Link to="./establishments">
-          <Button role="button">Establishments</Button>
-        </Link>
-      </Col>
+          <Link to="/enquiries" className="admin__section--link">
+            <Button role="button">Enquiries</Button>
+          </Link>
+          <Link to="./messages" className="admin__section--link">
+            <Button role="button">Messages</Button>
+          </Link>
+          <Link to="./establishments" className="admin__section--link">
+            <Button role="button">Establishments</Button>
+          </Link>
+        </Col>
 
-      <Col lg={6}>
-        <Image src={AdminImg} alt="aurora borealis" fluid role="img" />
-      </Col>
-    </Row>
+        <Col lg={6} className="admin__img">
+          <Image src={AdminImg} alt="aurora borealis" fluid role="img" />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
