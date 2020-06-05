91c5d72b656e55c4b6d6cf813b1a1c9cc7d6f8b5
ca2roaldsoy <roaldsoyen@gmail.com>
2020-06-05 21:45:23 +0200

    added classes to fetch error and access error

--- src/components/errorHandler/ErrorHandler.js
+++ src/components/errorHandler/ErrorHandler.js
@@ -6,16 +6,22 @@ import Container from "react-bootstrap/Container";
 function ErrorHandler() {
   return (
     <Container>
-      <Card>
+      <Card className="pageError">
         <Card.Img
           src={ErrorImg}
           alt="cartoon drawing, figure stuck in a messy cable"
           role="image"
         />
-        <Card.ImgOverlay>
-          <Card.Title>Oooopppppsss!!</Card.Title>
-          <Card.Text>Looks like something went wrong</Card.Text>
-          <Card.Text>Please try refresh the page</Card.Text>
+        <Card.ImgOverlay className="pageError__Containerr">
+          <Card.Title className="pageError__Container--title">
+            Oooopppppsss!!
+          </Card.Title>
+          <Card.Text className="pageError__Container--text">
+            Looks like something went wrong
+          </Card.Text>
+          <Card.Text className="pageError__Container--text">
+            Please try refresh the page
+          </Card.Text>
         </Card.ImgOverlay>
       </Card>
     </Container>
--- src/components/menu/Permission.js
+++ src/components/menu/Permission.js
@@ -2,27 +2,34 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 import Card from "react-bootstrap/Card";
 import StopSign from "../../images/stop.png";
+import Container from "react-bootstrap/Container";
 
 // if user has no permission to site,
 // display message and redirect user to home page
 function Permission() {
   const history = useHistory();
 
-  setTimeout(() => {
+  /*setTimeout(() => {
     history.push("/");
-  }, 5000);
+  }, 5000);*/
 
   return (
-    <Card>
-      <Card.Img src={StopSign} alt="man denying access" role="image" />
-      <Card.ImgOverlay>
-        <Card.Title>
-          Sorry, You dont have permission to view this page
-        </Card.Title>
-        <Card.Text>Please log in to gain access to this page</Card.Text>
-        <Card.Text>You will now be redirectd to home page</Card.Text>
-      </Card.ImgOverlay>
-    </Card>
+    <Container>
+      <Card className="pageError">
+        <Card.Img src={StopSign} alt="man denying access" role="image" />
+        <Card.ImgOverlay className="pageError__Container">
+          <Card.Title className="pageError__Container--title">
+            Sorry, You dont have permission to view this page
+          </Card.Title>
+          <Card.Text className="pageError__Container--text">
+            Please log in to gain access to this page
+          </Card.Text>
+          <Card.Text className="pageError__Container--text">
+            You will now be redirectd to home page
+          </Card.Text>
+        </Card.ImgOverlay>
+      </Card>
+    </Container>
   );
 }
 
