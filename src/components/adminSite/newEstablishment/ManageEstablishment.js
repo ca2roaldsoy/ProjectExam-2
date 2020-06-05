eaba3b4f0afc763044916be039b7c87f4c53b73e
ca2roaldsoy <roaldsoyen@gmail.com>
2020-06-05 16:50:37 +0200

    styled admin establishment menu

--- src/components/adminSite/newEstablishment/ManageEstablishment.js
+++ src/components/adminSite/newEstablishment/ManageEstablishment.js
@@ -1,17 +1,18 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import { NavLink } from "react-router-dom";
+import Container from "react-bootstrap/Container";
 
 function ManageEstablishment() {
   return (
-    <>
+    <Container className="manageEstablishmentMenu">
       <NavLink to="./create-establishments/">
         <Button>Create New Establishment</Button>
       </NavLink>
       <NavLink to="./delete-establishments/">
         <Button>Delete Establishment</Button>
       </NavLink>
-    </>
+    </Container>
   );
 }
 
