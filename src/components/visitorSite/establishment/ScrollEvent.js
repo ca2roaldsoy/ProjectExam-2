f1222bc006fdf48ce3f7e62cbdea0e25389cb220
ca2roaldsoy <roaldsoyen@gmail.com>
2020-06-09 12:47:20 +0200

    removed eventListener after unmounted component

--- src/components/visitorSite/establishment/ScrollEvent.js
+++ src/components/visitorSite/establishment/ScrollEvent.js
@@ -1,23 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 
 function ScrollEvent() {
   const [btnToTop, setBtnToTop] = useState(false);
 
-  const backToTop = () => {
-    if (window.pageYOffset <= 1000) {
-      setBtnToTop(false);
+  useEffect(() => {
+    window.addEventListener("scroll", scrollTop);
+
+    function scrollTop() {
+      if (window.pageYOffset <= 1000) {
+        setBtnToTop(false);
+      }
+      if (window.pageYOffset > 1000) {
+        setBtnToTop(true);
+      }
     }
-    if (window.pageYOffset > 1000) {
-      setBtnToTop(true);
-    }
-  };
+
+    return () => window.removeEventListener("scroll", scrollTop);
+  }, []);
 
   const topOfPage = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
-
-  window.addEventListener("scroll", backToTop);
 
   return (
     <Button
