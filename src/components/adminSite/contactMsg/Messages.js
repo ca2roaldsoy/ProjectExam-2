c100a52bf3a4329c1873c9fd5339f5cb60cc5245
ca2roaldsoy <roaldsoyen@gmail.com>
2020-06-10 16:29:27 +0200

    code cleanup

--- src/components/adminSite/contactMsg/Messages.js
+++ src/components/adminSite/contactMsg/Messages.js
@@ -82,7 +82,7 @@ function Messages() {
               </tr>
             </thead>
             <tbody>{noMsg()}</tbody>
-          </Table>{" "}
+          </Table>
         </>
       )}
     </Container>
