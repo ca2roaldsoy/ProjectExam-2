7a9678f60c402947327accc1ec8f29fbab6b5876
ca2roaldsoy <roaldsoyen@gmail.com>
2020-06-10 20:49:13 +0200

    changed breakpoints

--- src/constants/responsiveCarousel.js
+++ src/constants/responsiveCarousel.js
@@ -1,16 +1,16 @@
 export const Responsive = {
   lg: {
-    breakpoint: { max: 2048, min: 1024 },
+    breakpoint: { max: 2048, min: 992 },
     items: 3,
     slidesToSlide: 1,
   },
   md: {
-    breakpoint: { max: 1024, min: 768 },
+    breakpoint: { max: 992, min: 576 },
     items: 2,
     slidesToSlide: 1,
   },
   sm: {
-    breakpoint: { max: 768, min: 0 },
+    breakpoint: { max: 576, min: 0 },
     items: 1,
     slidesToSlide: 1,
   },
