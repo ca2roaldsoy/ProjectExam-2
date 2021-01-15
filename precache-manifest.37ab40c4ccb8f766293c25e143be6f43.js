5077bda67f6762b3a576a0cc32c249a7562564df
ca2roaldsoy <roaldsoyen@gmail.com>
2021-01-15 19:56:02 +0100

    Updates

---
+++ precache-manifest.37ab40c4ccb8f766293c25e143be6f43.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09b7a01f91f78acbb2328752e99dae54",
+    "url": "/ProjectExam-2/index.html"
+  },
+  {
+    "revision": "010ef327ebe6df263a14",
+    "url": "/ProjectExam-2/static/css/2.d6bddd92.chunk.css"
+  },
+  {
+    "revision": "8670a5f5fb0d62bda658",
+    "url": "/ProjectExam-2/static/css/main.c121ab7d.chunk.css"
+  },
+  {
+    "revision": "010ef327ebe6df263a14",
+    "url": "/ProjectExam-2/static/js/2.0f1da0e3.chunk.js"
+  },
+  {
+    "revision": "64f7e58d0b20d4d7a9a6188b47824605",
+    "url": "/ProjectExam-2/static/js/2.0f1da0e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8670a5f5fb0d62bda658",
+    "url": "/ProjectExam-2/static/js/main.f59e97bc.chunk.js"
+  },
+  {
+    "revision": "a7878e6f007289042554",
+    "url": "/ProjectExam-2/static/js/runtime-main.50ed40b1.js"
+  },
+  {
+    "revision": "8e674387cd68ad23ba5e5a714cdcc424",
+    "url": "/ProjectExam-2/static/media/Gloria.8e674387.ttf"
+  },
+  {
+    "revision": "af7c6eaa7d8e1c021f08bcb96d667287",
+    "url": "/ProjectExam-2/static/media/accept.af7c6eaa.png"
+  },
+  {
+    "revision": "ff9400da07becc3f678933218fbcbabf",
+    "url": "/ProjectExam-2/static/media/accept_white.ff9400da.png"
+  },
+  {
+    "revision": "350e948819d2cc0a82c426508c2a95df",
+    "url": "/ProjectExam-2/static/media/admin_page.350e9488.jpg"
+  },
+  {
+    "revision": "9935cac9832588b6c6d07bf962e10e6e",
+    "url": "/ProjectExam-2/static/media/bergen_blur_v4.9935cac9.jpg"
+  },
+  {
+    "revision": "500924b9576010b6e8e9a7ff481c873c",
+    "url": "/ProjectExam-2/static/media/bg-15.500924b9.jpg"
+  },
+  {
+    "revision": "d2ca8cc37227040f81b3ee7dd98e0008",
+    "url": "/ProjectExam-2/static/media/bryggen.d2ca8cc3.jpg"
+  },
+  {
+    "revision": "429de792d4b5a00f426689de83a21685",
+    "url": "/ProjectExam-2/static/media/fløibanen_v2.429de792.jpg"
+  },
+  {
+    "revision": "4f039f2f184480e2b59350bdc4841d8c",
+    "url": "/ProjectExam-2/static/media/information.4f039f2f.png"
+  },
+  {
+    "revision": "754085e8728c14766d5a7b04762b80c0",
+    "url": "/ProjectExam-2/static/media/reject_white.754085e8.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/ProjectExam-2/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/ProjectExam-2/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/ProjectExam-2/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "ae62be60d80b8b199d41391459c84280",
+    "url": "/ProjectExam-2/static/media/stop.ae62be60.png"
+  },
+  {
+    "revision": "491e1a0db0c696d58855cbf715959873",
+    "url": "/ProjectExam-2/static/media/trolltunga_v2.491e1a0d.jpg"
+  },
+  {
+    "revision": "81c2a708d09e266f4b926cff6ccf143e",
+    "url": "/ProjectExam-2/static/media/ulriken_v2.81c2a708.jpg"
+  }
+]);
